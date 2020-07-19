41943b0afd25cf8ae7bd7f5ffb9caaae7f35c550
zachjbrowning <zachjbrowning@outlook.com>
2020-07-19 17:53:57 +1000

    close to deployment

---
+++ build.sh
@@ -0,0 +1,2 @@
+rm -r dist
+yarn run build
--- src/Landing/Landing.js
+++ src/Landing/Landing.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default function Landing() {
     return <div className={`${styles.main} column is-10`}>
-        <h1 className="title is-1">Woahh.. hey there!</h1>
+        <div className="title is-1">Woahh.. hey there!</div>
+        <div className="title is-5">Welcome to my incredibly pathetic tribute to flash?? (RIP). I built this using REACT cause I was bored. Odds you can't get on the leaderboard for anything. </div>
     </div>
 }
