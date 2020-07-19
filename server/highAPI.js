b79a77c592554a1e2afeb4a5e76168af1b30bf62
zachjbrowning <zachjbrowning@outlook.com>
2020-07-19 17:48:06 +1000

    nearing completion

--- server/highAPI.js
+++ server/highAPI.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
 
+function insertScore(list, score) {
+    for (let i = 0; i < list.length; i++) {
+        if (score.score < list[i].score) {
+            list.splice(i, 0, score);
+            return;
+        }
+    }
+    list.push(score);
+}
+
 
 exports.getHighs = (game) => {
     var data = require('./highs.json');
@@ -9,6 +19,16 @@ exports.getHighs = (game) => {
 
 exports.add = (game, toSave) => {
     var data = require('./highs.json');
-    data[game].push(toSave);
+    
+    
+    if (data[game].length === 10) {
+        if (data[game][9].score > toSave.score) {
+            data[game].splice(9, 1);
+            insertScore(data[game], toSave);
+        }
+    } else {
+        insertScore(data[game], toSave);
+    }
+    //data[game].push(toSave);
     fs.writeFile('./server/highs.json', JSON.stringify(data), err => {if (err) console.log(err);})
 }
