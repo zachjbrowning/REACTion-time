d50c389340a72f52077bbe115067680cfe04e58b
zachjbrowning <zachjbrowning@outlook.com>
2020-07-19 18:05:35 +1000

    attempting deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const DIST_DIR = path.join(__dirname, '../dist');
 const APP_ENTRY = path.join(DIST_DIR, 'index.html');
 
